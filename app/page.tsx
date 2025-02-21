5a77d259cfdb802729401e723807e286b77f140b
Lovis Tawiah <lovistawiah638@gmail.com>
2025-02-21 10:38:10 -0800

    Remove Navbar in /page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import Contact from "@/components/Contact";
 import Events from "@/components/Events";
 import LandingPage from "@/components/LandingPage";
-import Navbar from "@/components/Navbar";
 import NotableSpeakers from "@/components/NotableSpeakers";
 import SeniorPastor from "@/components/SeniorPastor";
 import VisionMission from "@/components/VisionMission";
@@ -10,7 +9,6 @@ import WordsOfGrace from "@/components/WordsOfGrace";
 export default function Home() {
   return (
     <section>
-      <Navbar />
       <LandingPage />
       <Events />
       <VisionMission />
